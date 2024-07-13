c2e2887609a8053e39f9d357046e936c26229131
Lauta Ruslana <lauta.ruslana@ukr.net>
2024-07-13 20:56:42 +0300

    solution task 1

--- Lesson_30/script.js
+++ Lesson_30/script.js
@@ -1,3 +1,9 @@
 'use strict';
 /* Затримки 
 1. Напишіть функцію, яка приймає рядок і виводить його в консоль затримкою у 2 секунди. */
+let setTimeoutTwoSecond = str => {
+	setTimeout(() => {
+		console.log(str);
+	}, 2000);
+};
+setTimeoutTwoSecond('Hello World!');
