d5d4643ff5091c797072da074ebd1cf190190571
Lauta Ruslana <lauta.ruslana@ukr.net>
2024-07-13 21:18:27 +0300

    task 2

--- Lesson_30/script.js
+++ Lesson_30/script.js
@@ -1,9 +1,23 @@
 'use strict';
 /* Затримки 
 1. Напишіть функцію, яка приймає рядок і виводить його в консоль затримкою у 2 секунди. */
-let setTimeoutTwoSecond = str => {
+/* let setTimeoutTwoSecond = str => {
 	setTimeout(() => {
 		console.log(str);
 	}, 2000);
 };
-setTimeoutTwoSecond('Hello World!');
+setTimeoutTwoSecond('Hello World!'); */
+
+// 2. Створіть таймер, який відлічує час у зворотному напрямку від 10 до 0. Виведіть кожне число затримкою у 1 секунду.
+/* let timer = () => {
+	let count = 10;
+	let countDown = () => {
+		console.log(count);
+		count--;
+		if (count >= 0) {
+			setTimeout(countDown, 1000);
+		}
+	};
+	countDown();
+};
+timer(); */
